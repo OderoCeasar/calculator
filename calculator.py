00d3791bea7455e917adb82f7d79fb31a4d2aff0
Ceasar Odero <oderoceasar@gmail.com>
2025-03-13 21:44:44 +0300

    initial commit

---
+++ calculator.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2:"))
+
+sum = num1 + num2
+diff = num1 - num2
+multiply = num1 * num2
+division = num1 / num2
+
+print(f"Sum: {sum}")
+print(f"Diff: {diff}")  
+print(f"Multiply: {multiply}")
+print(f"Division: {division}")
